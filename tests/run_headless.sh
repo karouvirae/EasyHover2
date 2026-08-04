81869c413c252a5a476e89b7e7354df8c93044d8
maar-10 <m-krips@web.de>
2026-08-04 20:41:40 +0200

    feat(angle): shortest signed angle wrap

--- tests/run_headless.sh
+++ tests/run_headless.sh
@@ -9,7 +9,7 @@ cp -r "$ROOT/tests" "$COMP/"
 cat > "$COMP/startup.lua" <<'LUA'
 package.path = "/?.lua;/?/init.lua;" .. package.path
 local suites = { "tests.test_smoke", "tests.test_pid", "tests.test_pwm",
-                 "tests.test_mixer", "tests.test_sim", "tests.test_integration" }
+                 "tests.test_mixer", "tests.test_sim", "tests.test_integration", "tests.test_angle" }
 local t = require("tests.framework")
 local loadErrs = {}
 for _, s in ipairs(suites) do
